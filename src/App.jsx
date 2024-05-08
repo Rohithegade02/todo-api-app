e2ad8972aef67568610ae25d7f0af924d676db42
Rohit Hegade <60090023+Rohithegade02@users.noreply.github.com>
2024-05-08 08:11:20 +0530

    added isauth

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
-import {
-  BrowserRouter,
-  Navigate,
-  Outlet,
-  Route,
-  Routes,
-} from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import TodoList from './components/TodoList'
 import SignUp from './components/SignUp'
 import Login from './components/Login'
 import { useSelector } from 'react-redux'
-import PrivateOutlet from './components/PrivateOutlet'
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
+
   return (
     <div className='max-w-[100vw]'>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<SignUp />} />
           <Route path='/login' element={<Login />} />
-          {/* Private route only can be accessed using token */}
+          {/* Private route only can be accessed when authenticated */}
           <Route
             path='/todo'
             element={
-              <PrivateOutlet>
+              <PrivateRoute isAuthenticated={isAuthenticated}>
                 <TodoList />
-              </PrivateOutlet>
+              </PrivateRoute>
+            }
+          />
+          {/* Redirect to login if not authenticated */}
+          <Route
+            path='/todo'
+            element={
+              !isAuthenticated ? <Navigate to='/login' replace /> : <TodoList />
             }
           />
         </Routes>
